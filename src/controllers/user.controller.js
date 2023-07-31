812ed2bd3ba618ed336bd475be25aa32a301e03e
Avi <avi.ruiz@proton.me>
2023-07-31 13:40:52 -0600

    view posts current, previous and next for user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,4 +52,55 @@ const followUser = async (req, res, next) => {
   }
 };
 
-export { viewProfile, followUser };
+const viewPost = async (req, res, next) => {
+  const { id, post } = req.params;
+  const user = req.user;
+
+  const userID = Number(id || user.id);
+
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: { userID },
+      include: { user: true, media: true, profile: true, votes: true },
+      orderBy: { createdAt: "asc" },
+    });
+
+    const currentIndex = userPosts.findIndex((p) => p.id === Number(post));
+
+    if (currentIndex === -1) {
+      throw new Error(
+        "El ID de publicación especificado no corresponde a un post del usuario."
+      );
+    }
+
+    const currentPost = userPosts[currentIndex];
+    const previousPost = currentIndex > 0 ? userPosts[currentIndex - 1] : null;
+    const nextPost =
+      currentIndex < userPosts.length - 1 ? userPosts[currentIndex + 1] : null;
+
+    res.json({
+      previousPost: previousPost.id,
+      currentPost: {
+        id: currentPost.id,
+        userID: currentPost.user.id,
+        username: currentPost.user.username,
+        photo:
+          currentPost.profile.photo === null
+            ? null
+            : `http://${req.get("host")}/images/${currentPost.profile.photo}`,
+        media: currentPost.media.map(
+          (media) => `http://${req.get("host")}/images/${media.resource}`
+        ),
+        description: currentPost.body,
+        date: currentPost.createdAt,
+        votes: currentPost.votes.length,
+        isVoted: currentPost.votes.some((v) => v.userID === user.id),
+      },
+      nextPost: nextPost.id,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { viewProfile, followUser, viewPost };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import authenticate from "../middleware/authenticate.js";
-import { viewProfile, followUser } from "../controllers/user.controller.js";
+import {
+  viewProfile,
+  followUser,
+  viewPost,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get("/:id?", authenticate, viewProfile);
-router.get("/:id/follow", authenticate, followUser)
+router.get("/:id/follow", authenticate, followUser);
+router.get("/:id?/post/:post?", authenticate, viewPost);
 
 export default router;
