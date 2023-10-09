45bb98e14c2d8f8666925eaa91e7f98e9db73fe9
ruizavi <avi.ruiz@proton.me>
2023-10-08 20:50:57 -0600

    add error message in errorhandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,9 +2,9 @@ function errorHandler(err, req, res, next) {
   if (res.headersSent) {
     return next(err);
   }
-  console.log(err)
+  console.log(err);
   res.status(500);
-  res.json({ error: err });
+  res.json({ error: err, message: err.message });
 }
 
 export default errorHandler;
